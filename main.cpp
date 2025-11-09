b0c6703ac924d0f0a1ae9c0cdcb93e6cab1635e5
ozymandias <megalop@eta.com>
2025-11-09 20:02:00 +0100

    v0.6.5

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <cstring>
 
 char pixels[6][9]={
-  {'A', 'g', 'g',
-   'A', 'g', 'A',
-   'A', 'g', 'g'},
+  {'g', 'g', 'g',
+   'g', 'g', 'g',
+   'g', 'g', 'g'},
 
   {'w', 'w', 'w',
    'w', 'w', 'w',
@@ -18,32 +18,36 @@ char pixels[6][9]={
    'y', 'y', 'y',
    'y', 'y', 'y'},
 
-  {'o', 'o', 'A',
-   'o', 'A', 'A',
-   'o', 'o', 'A'},
+  {'o', 'o', 'o',
+   'o', 'o', 'o',
+   'o', 'o', 'o'},
 
-  {'A', 'r', 'r',
-   'A', 'A', 'r',
+  {'r', 'r', 'r',
+   'r', 'r', 'r',
    'A', 'r', 'r'},
 };
 
-char* line[24];
+char* line[52];
 char staticLine[12];
 char lateral[9];
-
-char left[2] = {0, 4};
-char right[2] = {12, 5};
 
 char permDown[12] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2};
 char permUp[12] = {9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-char face [9];
-
 char permLateralDown[9] = {6, 3, 0, 7, 4, 1, 8, 5, 2};
 char permLateralUp[9] = {2, 5, 8, 1, 4, 7, 0, 3, 6};
 
-char* up [2] = {permUp, permLateralUp};
-char* down [2] = {permDown, permLateralDown};
+char permUD[4] = {0, 4, 2, 5};
+char permFB[4] = {1, 5, 3, 4};
+
+char permULine[12] = {0, 1, 2, 0, 1, 2, 8, 7, 6, 0, 1, 2};
+char permDLine[12] = {6, 7, 8, 6, 7, 8, 2, 1, 0, 6, 7, 8};
+
+
+char pemBLine[12] = {0, 1, 2, 2, 5, 8, 8, 7, 6, 6, 3, 0};
+char pemFLine[12] = {6, 7, 8, 0, 3, 6, 8, 7, 6, 8, 5, 2};
+
+char face [9];
 
 static inline void spinLateral (char side, char perm[9]) {
   memcpy(lateral, pixels[side], sizeof(lateral));
@@ -58,6 +62,10 @@ static inline void savesLine () {
     for (char j = 0; j < 7; j += 3) {
       line[k] = &pixels[i][j];
       line[k+12] = &pixels[i][j+2];
+      line[k+24]= &pixels[permUD[i]][permULine[k]];
+      line[k+36]= &pixels[permUD[i]][permDLine[k]];
+      line[k+48]= &pixels[permFB[i]][pemFLine[k]];
+      line[k+52]= &pixels[permFB[i]][pemBLine[k]];
       k++;
     }
   }
@@ -72,32 +80,7 @@ static inline void spinLine (char side, char perm[12]) {
   }
 }
 
-static inline void fullBackSpinBackSpin () {
-    memcpy(face, pixels[0], sizeof(face));
-    memcpy(pixels[0], pixels[1], sizeof(face));
-    memcpy(pixels[1], pixels[2], sizeof(face));
-    memcpy(pixels[2], pixels[3], sizeof(face));
-    memcpy(pixels[3], face, sizeof(face));
 
-    spinLateral(4, permLateralDown);
-    spinLateral(5, permLateralUp);
-}
-
-static inline void fullFrontSpin () {
-    memcpy(face, pixels[3], sizeof(face));
-    memcpy(pixels[3], pixels[2], sizeof(face));
-    memcpy(pixels[2], pixels[1], sizeof(face));
-    memcpy(pixels[1], pixels[0], sizeof(face));
-    memcpy(pixels[0], face, sizeof(face)); 
-    
-    spinLateral(4, permLateralUp);
-    spinLateral(5, permLateralDown);
-}
-
-static inline void lr(char side[2], char* direction [2]) {
-  spinLine(side[0], direction[0]);
-  spinLateral(side[1], direction[1]);
-}
 
 void print () {
   char k = 0;
@@ -116,7 +99,5 @@ void print () {
 
 int main () {
   savesLine();
-  //fullFrontSpin();
-  lr(left, down);
   print();
 }
