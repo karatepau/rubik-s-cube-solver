7e287b60d2bac04f05b6399b773f4c4aac63cb1e
ozymandias <megalop@eta.com>
2025-11-10 15:41:40 +0100

    v0.7.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstring>
 
+//3d cube represented with a 2d matrix, 6 faces with 9 colors/pixels inside each of one
 char pixels[6][9]={
   {'g', 'g', 'g',
    'g', 'g', 'g',
@@ -27,27 +28,29 @@ char pixels[6][9]={
    'r', 'r', 'r'},
 };
 
+//Save of all the lines (edges+corners), his static copy (the same but without being a pointer) and a copy for sides
 char* line[52];
 char staticLine[12];
 char lateral[9];
 
+//Permutation of the lines that are going to rotate
 char permDown[12] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2};
 char permUp[12] = {9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8};
 
+//Permutation of the laterals when they are rotated
 char permLateralDown[9] = {6, 3, 0, 7, 4, 1, 8, 5, 2};
 char permLateralUp[9] = {2, 5, 8, 1, 4, 7, 0, 3, 6};
 
+//List of indexes (of the faces) to follow in the loop for saving the line
 char permUD[4] = {0, 4, 2, 5};
 char permFB[4] = {1, 5, 3, 4};
 
+//Order of saving the pixels inside the line
 char permULine[12] = {0, 1, 2, 0, 1, 2, 8, 7, 6, 0, 1, 2};
 char permDLine[12] = {6, 7, 8, 6, 7, 8, 2, 1, 0, 6, 7, 8};
 
-
 char permBLine[12] = {0, 1, 2, 2, 5, 8, 8, 7, 6, 6, 3, 0};
 char pemrFLine[12] = {6, 7, 8, 0, 3, 6, 8, 7, 6, 8, 5, 2};
-
-char face [9];
 
 static inline void spinLateral (char side, char perm[9]) {
   memcpy(lateral, pixels[side], sizeof(lateral));
@@ -79,7 +82,6 @@ static inline void spinLine (char side, char perm[12]) {
     *line[i+side]=staticLine[perm[i]];
   }
 }
-
 
 
 void print () {
